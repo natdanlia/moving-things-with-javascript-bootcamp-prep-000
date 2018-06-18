61cd3dfddab073a75d5acf39dd49920ee405e254
Nathan Gezahegne <nathandaniel2222@gmail.com>
2018-06-18 15:00:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,3 +17,11 @@ document.addEventListener('keydown', function(e) {
   }
 })
 
+function moveDodgerRight(){
+	var rightNumbers = dodger.style.left.replace('px', '');
+	var right = parseInt(rightNumbers, 10)
+	
+	if(right < 360){
+		dodger.style.left = `${right + 1}px`
+	}
+}
